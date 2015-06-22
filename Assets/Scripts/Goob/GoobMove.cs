a681018843fe9c58787d440d56731f2ec2be439c
Martin Chuckeles <martin@jakubik.info>
2015-06-23 01:14:06 +0200

    Added speed variables to GoobMove

--- Assets/Scripts/Goob/GoobMove.cs
+++ Assets/Scripts/Goob/GoobMove.cs
@@ -1,5 +1,34 @@
 ﻿using UnityEngine;
 
+/// <summary>
+///   Handles the goob movement and collisions.
+/// </summary>
 public class GoobMove : MonoBehaviour {
+  #region Types
 
+  public enum MovementDirection {
+
+    Horizontal,
+    Vertical
+
+  }
+
+  #endregion
+
+  #region Methods
+
+  public void Move(MovementDirection movementDirection) {}
+
+  #endregion
+
+  #region Fields
+
+  // movement speed of the goob
+  public float MovementSpeed = 1f;
+
+  // actual speed of the goob
+  private float mHorizontalSpeed = 0;
+  private float mVerticalSpeed = 0;
+
+  #endregion
 }

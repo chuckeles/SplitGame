d0926a57bc7f3f0d74ea2d0582a1a3234383a22a
Martin Chuckeles <martin@jakubik.info>
2015-06-23 00:29:40 +0200

    Tracking mouse clicking

--- Assets/Scripts/Goob/GoobSplit.cs
+++ Assets/Scripts/Goob/GoobSplit.cs
@@ -5,8 +5,18 @@
 /// </summary>
 public class GoobSplit : MonoBehaviour {
 
+  private bool mHold = false;
+
   public void OnMouseOver() {
-    Debug.Log("The mouse is over!");
+    // check the mouse
+    if (Input.GetButtonDown("Mouse 0")) {
+      mHold = true;
+      Debug.Log("Pressed");
+    }
+    else if (Input.GetButtonUp("Mouse 0")) {
+      mHold = false;
+      Debug.Log("Released");
+    }
   }
 
 }

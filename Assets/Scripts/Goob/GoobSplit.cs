ca8969b900b42e06febfe5eda186a356e87083db
Martin Chuckeles <martin@jakubik.info>
2015-06-23 01:05:24 +0200

    Moved release check to Update

--- Assets/Scripts/Goob/GoobSplit.cs
+++ Assets/Scripts/Goob/GoobSplit.cs
@@ -8,11 +8,15 @@ public class GoobSplit : MonoBehaviour {
 
   public void OnMouseOver() {
     // check the mouse
-    if (Input.GetButtonDown("Mouse 0")) {
+    if (!mHold && Input.GetButtonDown("Mouse 0")) {
       mHold = true;
       Debug.Log("Pressed");
     }
-    else if (Input.GetButtonUp("Mouse 0")) {
+  }
+
+  public void Update() {
+    // check the mouse
+    if (mHold && Input.GetButtonUp("Mouse 0")) {
       mHold = false;
       Debug.Log("Released");
     }

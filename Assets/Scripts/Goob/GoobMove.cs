c1bd612d4aec8f2d112a098f86cc3671509b3367
Martin Chuckeles <martin@jakubik.info>
2015-06-23 18:27:19 +0200

    Added Button script

--- Assets/Scripts/Goob/GoobMove.cs
+++ Assets/Scripts/Goob/GoobMove.cs
@@ -11,20 +11,24 @@ public class GoobMove : MonoBehaviour {
   /// </summary>
   /// <param name="movementDirection">Desired movement direction</param>
   public void Move(MovementDirection movementDirection) {
-    if (mMovementDirection != MovementDirection.None) {
+    if (MovementDirection != MovementDirection.None) {
       // we are already moving
       return;
     }
 
     // start moving in the direction
-    mMovementDirection = movementDirection;
+    MovementDirection = movementDirection;
+  }
+
+  public void Start() {
+    MovementDirection = MovementDirection.None;
   }
 
   /// <summary>
   ///   Stops the goob.
   /// </summary>
   public void Stop() {
-    mMovementDirection = MovementDirection.None;
+    MovementDirection = MovementDirection.None;
   }
 
   /// <summary>
@@ -54,7 +58,7 @@ public class GoobMove : MonoBehaviour {
     mPreviousPosition = transform.position;
 
     // move the goob
-    switch (mMovementDirection) {
+    switch (MovementDirection) {
       case MovementDirection.Up:
         transform.Translate(0, MovementSpeed * Time.deltaTime, 0);
         break;
@@ -81,8 +85,13 @@ public class GoobMove : MonoBehaviour {
   ///   Returns true if the goob is moving.
   /// </summary>
   public bool IsMoving {
-    get { return mMovementDirection != MovementDirection.None; }
+    get { return MovementDirection != MovementDirection.None; }
   }
+
+  /// <summary>
+  ///   Returns the movement direction of the goob.
+  /// </summary>
+  public MovementDirection MovementDirection { get; private set; }
 
   #endregion
 
@@ -90,9 +99,6 @@ public class GoobMove : MonoBehaviour {
 
   // movement speed of the goob
   public float MovementSpeed = 2f;
-
-  // actual movement of the goob
-  private MovementDirection mMovementDirection = MovementDirection.None;
 
   // previous position
   private Vector3 mPreviousPosition;

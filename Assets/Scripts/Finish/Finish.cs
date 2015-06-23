72e9d6fcd894cfe13e6b7adbea810a3d85364f76
Martin Chuckeles <martin@jakubik.info>
2015-06-23 12:49:19 +0200

    Added Finish script

---
+++ Assets/Scripts/Finish/Finish.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Manages the finish block.
+/// </summary>
+public class Finish : MonoBehaviour {
+  #region Methods
+
+  public void Update() {
+    // check for a goob on me
+    Collider2D goob = Physics2D.OverlapPoint(transform.position);
+
+    // set the is activated
+    IsActivated = (goob && !goob.GetComponent<GoobMove>().IsMoving);
+  }
+
+  #endregion
+
+  #region Properties
+
+  /// <summary>
+  ///   Returns true is there's a stationary goob on the finish block.
+  /// </summary>
+  public bool IsActivated { get; private set; }
+
+  #endregion
+}

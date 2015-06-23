1b58986d9731b7063f22a4efe7d3d6108f8e73c7
Martin Chuckeles <martin@jakubik.info>
2015-06-23 16:48:05 +0200

    Added PushArea script

---
+++ Assets/Scripts/Interactible/PushArea.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Pushes stationary goobs in it's direction.
+/// </summary>
+public class PushArea : MonoBehaviour {
+  #region Methods
+
+  public void OnTriggerEnter2D(Collider2D collision) {
+    // check it is goob
+    if (collision.tag != "Goob")
+      return;
+
+    // get direction
+    float direction = transform.rotation.eulerAngles.z;
+
+    // "normalize"
+    while (direction < 0)
+      direction += 360;
+    while (direction > 360)
+      direction -= 360;
+
+    // round
+    direction = Mathf.Round(direction);
+
+    // transform
+    MovementDirection movementDirection;
+
+    if (direction < 10)
+      movementDirection = MovementDirection.Right;
+    else if (direction < 100)
+      movementDirection = MovementDirection.Up;
+    else if (direction < 190)
+      movementDirection = MovementDirection.Left;
+    else
+      movementDirection = MovementDirection.Down;
+
+    // apply movement
+    collision.GetComponent<GoobMove>().Move(movementDirection);
+  }
+
+  #endregion
+}

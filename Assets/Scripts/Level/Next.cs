718981227b82283c428989e520c75cf125941ff7
Martin Chuckeles <martin@jakubik.info>
2015-06-23 13:14:32 +0200

    Next button now checks finish blocks and moves to the next level

--- Assets/Scripts/Level/Next.cs
+++ Assets/Scripts/Level/Next.cs
@@ -1,5 +1,25 @@
 ﻿using UnityEngine;
 
 public class Next : MonoBehaviour {
+  #region Methods
 
+  public void GoToNextLevel() {
+    // check the finish blocks
+    GameObject[] finishBlocks = GameObject.FindGameObjectsWithTag("Finish");
+
+    var weCanGo = true;
+    foreach (GameObject finish in finishBlocks) {
+      if (!finish.GetComponent<Finish>().IsActivated) {
+        // this one is not active
+        weCanGo = false;
+        break;
+      }
+    }
+
+    // transition
+    if (weCanGo)
+      Application.LoadLevel(Application.loadedLevel + 1);
+  }
+
+  #endregion
 }

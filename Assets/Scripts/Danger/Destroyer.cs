23e533497dde6d8711e15ee13231f5a27a18e980
Martin Chuckeles <martin@jakubik.info>
2015-06-23 13:47:04 +0200

    Added Destroyer script and the Spikes

---
+++ Assets/Scripts/Danger/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Destroyes a goob on touch.
+/// </summary>
+public class Destroyer : MonoBehaviour {
+  #region Methods
+
+  public void OnTriggerEnter2D(Collider2D collision) {
+    // is it a goob?
+    if (collision.tag == "Goob") {
+      // destroy it
+      Destroy(collision.gameObject);
+    }
+
+  }
+
+  #endregion
+}

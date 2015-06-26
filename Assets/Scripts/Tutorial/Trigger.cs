75fc9adeb477d5c1bb88c5e46f13b211028e1d2d
Martin Chuckeles <martin@jakubik.info>
2015-06-26 23:01:24 +0200

    Added a trigger

---
+++ Assets/Scripts/Tutorial/Trigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   On the trigger stops one animation and starts another one.
+/// </summary>
+public class Trigger : MonoBehaviour {
+  #region Methods
+
+  public void OnTriggerEnter2D(Collider2D collision) {
+    // check if it is a goob
+    if (collision.tag == "Goob") {
+      // update animations
+      AnimationToStop.Stop();
+      AnimationToStart.Play();
+    }
+  }
+
+  #endregion
+
+  #region Fields
+
+  public Animation AnimationToStart;
+  public Animation AnimationToStop;
+
+  #endregion
+}

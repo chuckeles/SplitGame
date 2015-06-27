43367333894471c8522ab93b047f4a286f8d8a05
Martin Chuckeles <martin@jakubik.info>
2015-06-27 10:26:07 +0200

    Added TrackRestart script

--- Assets/Scripts/Analytics/InitializeAnalytics.cs
+++ Assets/Scripts/Analytics/InitializeAnalytics.cs
@@ -7,8 +7,14 @@ public class InitializeAnalytics : MonoBehaviour {
   #region Methods
 
   public void Start() {
-    var infinario = new Infinario.Infinario("174bedd6-1ca3-11e5-9063-b083fedeed2e");
+    Infinario = new Infinario.Infinario("174bedd6-1ca3-11e5-9063-b083fedeed2e");
   }
+
+  #endregion
+
+  #region Properties
+
+  public Infinario.Infinario Infinario { get; private set; }
 
   #endregion
 }
---
+++ Assets/Scripts/Analytics/TrackRestart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///   Tracks the restart level event.
+/// </summary>
+public class TrackRestart : MonoBehaviour {
+  #region Methods
+
+  /// <summary>
+  ///   Sends the tracked event.
+  /// </summary>
+  public void Track() {
+    MainCameraSingleton.Instance.GetComponent<InitializeAnalytics>()
+      .Infinario.Track("restart", new Dictionary<string, object> {
+        {"level", Application.loadedLevelName}
+      });
+  }
+
+  #endregion
+}

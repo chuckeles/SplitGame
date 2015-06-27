652157d0364ab426b7534385d9c6ed102c220144
Martin Chuckeles <martin@jakubik.info>
2015-06-27 11:17:59 +0200

    Added TrackGoob

---
+++ Assets/Scripts/Analytics/TrackGoob.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///   Tracks all goob events.
+/// </summary>
+public class TrackGoob : MonoBehaviour {
+  #region Methods
+
+  /// <summary>
+  ///   Tracks the destroy event.
+  /// </summary>
+  public void TrackDestroy() {
+    MainCameraSingleton.Instance.GetComponent<InitializeAnalytics>()
+      .Infinario.Track("goob", new Dictionary<string, object> {
+        {"action", "destroy"}
+      });
+  }
+
+  /// <summary>
+  ///   Tracks the merge event.
+  /// </summary>
+  public void TrackMerge() {
+
+    MainCameraSingleton.Instance.GetComponent<InitializeAnalytics>()
+      .Infinario.Track("goob", new Dictionary<string, object> {
+        {"action", "merge"}
+      });
+  }
+
+  /// <summary>
+  ///   Tracks the split event.
+  /// </summary>
+  public void TrackSplit(string type) {
+
+    MainCameraSingleton.Instance.GetComponent<InitializeAnalytics>()
+      .Infinario.Track("goob", new Dictionary<string, object> {
+        {"action", "split"},
+        {"type", type}
+      });
+  }
+
+  #endregion
+}

5bdcdc18258e3d34fa821d178666587cd7d1fcc3
Martin Chuckeles <martin@jakubik.info>
2015-06-27 10:19:01 +0200

    Added script to initialize Infinario

---
+++ Assets/Scripts/Analytics/InitializeAnalytics.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Initializes Infinario analytics.
+/// </summary>
+public class InitializeAnalytics : MonoBehaviour {
+  #region Methods
+
+  public void Start() {
+    var infinario = new Infinario.Infinario("174bedd6-1ca3-11e5-9063-b083fedeed2e");
+  }
+
+  #endregion
+}

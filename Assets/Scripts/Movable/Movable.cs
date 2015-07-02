70e7eac4ba16093ca757f0be655d20d86ad217d2
Martin Chuckeles <martin@jakubik.info>
2015-07-02 15:15:22 +0200

    Fixed movable script

--- Assets/Scripts/Movable/Movable.cs
+++ Assets/Scripts/Movable/Movable.cs
@@ -1,129 +1,133 @@
 ﻿using UnityEngine;
 
-/// <summary>
-///   Handles the goob movement and collisions.
-/// </summary>
-public class GoobMove : MonoBehaviour {
-  #region Constructor
-
-  public GoobMove() {
-    MovementDirection = MovementDirection.None;
-  }
-
-  #endregion
-
-  #region Methods
+namespace SplitGame {
 
   /// <summary>
-  ///   Moves the goob in a direction, if it's not moving already.
+  ///   Handles the goob movement and collisions.
   /// </summary>
-  /// <param name="movementDirection">Desired movement direction</param>
-  public void Move(MovementDirection movementDirection) {
-    if (MovementDirection != MovementDirection.None) {
-      // we are already moving
-      return;
+  public class GoobMove : MonoBehaviour {
+    #region Constructor
+
+    public GoobMove() {
+      MovementDirection = MovementDirection.None;
     }
 
-    // start moving in the direction
-    MovementDirection = movementDirection;
-  }
+    #endregion
 
-  /// <summary>
-  ///   Stops the goob.
-  /// </summary>
-  public void Stop() {
-    MovementDirection = MovementDirection.None;
-  }
+    #region Methods
 
-  /// <summary>
-  ///   Stops the goob and aligns it to a 1x1 grid.
-  /// </summary>
-  public void StopAndAlign() {
-    Stop();
+    /// <summary>
+    ///   Moves the goob in a direction, if it's not moving already.
+    /// </summary>
+    /// <param name="movementDirection">Desired movement direction</param>
+    public void Move(MovementDirection movementDirection) {
+      if (MovementDirection != MovementDirection.None) {
+        // we are already moving
+        return;
+      }
 
-    // align
-    var newPosition = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y),
-                                  transform.position.z);
-    transform.position = newPosition;
-
-    // check my position for other goobs
-    CheckGoobs();
-  }
-
-  public void Update() {
-    // update previous position
-
-    // move the goob
-    switch (MovementDirection) {
-      case MovementDirection.Up:
-        transform.Translate(0, MovementSpeed * Time.deltaTime, 0);
-        break;
-
-      case MovementDirection.Right:
-        transform.Translate(MovementSpeed * Time.deltaTime, 0, 0);
-        break;
-
-      case MovementDirection.Down:
-        transform.Translate(0, -MovementSpeed * Time.deltaTime, 0);
-        break;
-
-      case MovementDirection.Left:
-        transform.Translate(-MovementSpeed * Time.deltaTime, 0, 0);
-        break;
+      // start moving in the direction
+      MovementDirection = movementDirection;
     }
-  }
 
-  /// <summary>
-  ///   Checks the position for other stationary goobs.
-  /// </summary>
-  private void CheckGoobs() {
-    // get goobs
-    GameObject[] goobs = GameObject.FindGameObjectsWithTag("Goob");
+    /// <summary>
+    ///   Stops the goob.
+    /// </summary>
+    public void Stop() {
+      MovementDirection = MovementDirection.None;
+    }
 
-    // check them
-    foreach (GameObject goob in goobs) {
-      // is it me?
-      if (goob == gameObject)
-        continue;
+    /// <summary>
+    ///   Stops the goob and aligns it to a 1x1 grid.
+    /// </summary>
+    public void StopAndAlign() {
+      Stop();
 
-      // is it moving?
-      if (goob.GetComponent<GoobMove>().IsMoving)
-        continue;
+      // align
+      var newPosition = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y),
+                                    transform.position.z);
+      transform.position = newPosition;
 
-      // check position
-      var deltaPos = new Vector2(Mathf.Abs(transform.position.x - goob.transform.position.x),
-                                 Mathf.Abs(transform.position.y - goob.transform.position.y));
+      // check my position for other goobs
+      CheckGoobs();
+    }
 
-      if (Mathf.Max(deltaPos.x, deltaPos.y) < 0.2f) {
-        // same grid cell, self-destruction imminent
-        Destroy(gameObject);
-        break;
+    public void Update() {
+      // update previous position
+
+      // move the goob
+      switch (MovementDirection) {
+        case MovementDirection.Up:
+          transform.Translate(0, MovementSpeed * Time.deltaTime, 0);
+          break;
+
+        case MovementDirection.Right:
+          transform.Translate(MovementSpeed * Time.deltaTime, 0, 0);
+          break;
+
+        case MovementDirection.Down:
+          transform.Translate(0, -MovementSpeed * Time.deltaTime, 0);
+          break;
+
+        case MovementDirection.Left:
+          transform.Translate(-MovementSpeed * Time.deltaTime, 0, 0);
+          break;
       }
     }
+
+    /// <summary>
+    ///   Checks the position for other stationary goobs.
+    /// </summary>
+    private void CheckGoobs() {
+      // get goobs
+      GameObject[] goobs = GameObject.FindGameObjectsWithTag("Goob");
+
+      // check them
+      foreach (GameObject goob in goobs) {
+        // is it me?
+        if (goob == gameObject)
+          continue;
+
+        // is it moving?
+        if (goob.GetComponent<GoobMove>().IsMoving)
+          continue;
+
+        // check position
+        var deltaPos = new Vector2(Mathf.Abs(transform.position.x - goob.transform.position.x),
+                                   Mathf.Abs(transform.position.y - goob.transform.position.y));
+
+        if (Mathf.Max(deltaPos.x, deltaPos.y) < 0.2f) {
+          // same grid cell, self-destruction imminent
+          Destroy(gameObject);
+          break;
+        }
+      }
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    ///   Returns true if the goob is moving.
+    /// </summary>
+    public bool IsMoving {
+      get { return MovementDirection != MovementDirection.None; }
+    }
+
+    /// <summary>
+    ///   Returns the movement direction of the goob.
+    /// </summary>
+    public MovementDirection MovementDirection { get; private set; }
+
+    #endregion
+
+    #region Fields
+
+    // movement speed of the goob
+    public float MovementSpeed = 2f;
+
+    #endregion
   }
 
-  #endregion
-
-  #region Properties
-
-  /// <summary>
-  ///   Returns true if the goob is moving.
-  /// </summary>
-  public bool IsMoving {
-    get { return MovementDirection != MovementDirection.None; }
-  }
-
-  /// <summary>
-  ///   Returns the movement direction of the goob.
-  /// </summary>
-  public MovementDirection MovementDirection { get; private set; }
-
-  #endregion
-
-  #region Fields
-
-  // movement speed of the goob
-  public float MovementSpeed = 2f;
-
-  #endregion
 }

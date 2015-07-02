a00241b843a2be6364136b93edf63ea9cf95a988
Martin Chuckeles <martin@jakubik.info>
2015-07-02 15:16:18 +0200

    Fixed Collision script

--- Assets/Scripts/Movable/Collision.cs
+++ Assets/Scripts/Movable/Collision.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
 
-/// <summary>
-///   Stops the goob on collision with a static object.
-/// </summary>
-[RequireComponent(typeof (GoobMove))]
-public class GoobCollision : MonoBehaviour {
-  #region Methods
+namespace SplitGame {
 
-  public void OnTriggerEnter2D(Collider2D collision) {
-    // is it a wall?
-    if (collision.tag == "Wall") {
-      // stop
-      GetComponent<GoobMove>().StopAndAlign();
+  /// <summary>
+  ///   Stops the goob on collision with a static object.
+  /// </summary>
+  [RequireComponent(typeof (GoobMove))]
+  public class GoobCollision : MonoBehaviour {
+    #region Methods
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+      // is it a wall?
+      if (collision.tag == "Wall") {
+        // stop
+        GetComponent<GoobMove>().StopAndAlign();
+      }
     }
+
+    public void Start() {
+      // test requirements
+      if (GetComponent<GoobMove>() == null)
+        throw new MissingComponentException("GoobMove is required but not attached");
+    }
+
+    #endregion
   }
 
-  public void Start() {
-    // test requirements
-    if (GetComponent<GoobMove>() == null)
-      throw new MissingComponentException("GoobMove is required but not attached");
-  }
-
-  #endregion
 }

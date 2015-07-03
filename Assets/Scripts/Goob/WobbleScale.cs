832feeae0ac6d2d43c5276a3d4bcda038506f33d
Martin Chuckeles <martin@jakubik.info>
2015-07-03 10:41:06 +0200

    Added WobbleScale

---
+++ Assets/Scripts/Goob/WobbleScale.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/// <summary>
+///   Handles the scale of the object in a wobbly style.
+/// </summary>
+public class WobbleScale : MonoBehaviour {
+  #region Methods
+
+  public void Start() {
+    TargetScale = transform.localScale;
+  }
+
+  public void Update() {
+    // get delta
+    Vector2 delta = TargetScale - (Vector2) transform.localScale;
+
+    // add to the speed
+    ScaleSpeed += delta * ScaleSpeedChangePercent;
+
+    // apply friction
+    ScaleSpeed *= (1f - ScaleSpeedFriction);
+
+    // apply to the scale
+    transform.localScale += (Vector3) ScaleSpeed * Time.deltaTime;
+  }
+
+  #endregion
+
+  #region Fields
+
+  public Vector2 ScaleSpeed;
+  public float ScaleSpeedFriction = 0.05f;
+  public float ScaleSpeedChangePercent = 0.1f;
+  public Vector2 TargetScale;
+
+  #endregion
+}

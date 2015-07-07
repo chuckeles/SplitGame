d6bf893d7723a04c4dc7333d6a0e1d0c3b4de98e
Martin Chuckeles <martin@jakubik.info>
2015-07-07 13:44:12 +0200

    Added collision wobble, closes #26

---
+++ Assets/Scripts/Goob/CollisionWobble.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace SplitGame {
+
+  /// <summary>
+  ///   Wobbles the goob on mouse hover.
+  /// </summary>
+  [RequireComponent(typeof (WobbleScale))]
+  public class CollisionWobble : MonoBehaviour {
+    #region Methods
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+      // is it a wall?
+      if (collision.tag == "Wall") {
+        // wobble
+        GetComponent<WobbleScale>().Wobble(WobbleForce);
+      }
+    }
+
+    #endregion
+
+    #region Fields
+
+    public float WobbleForce = 1.0f;
+
+    #endregion
+  }
+
+}
